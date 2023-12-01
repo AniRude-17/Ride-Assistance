e1cfb36056d144257f4fff8ee047cb73b126de21
anir-ab <anibhagwat17@gmail.com>
2023-12-01 17:42:24 +0530

    Initialize project folder

---
+++ backend/app.js
@@ -0,0 +1,12 @@
+const express = require('express'); 
+  
+const app = express(); 
+const PORT = 5000; 
+  
+app.listen(PORT, (error) =>{ 
+    if(!error) 
+        console.log("SERVER ON") 
+    else 
+        console.log("ERROR", error); 
+    } 
+); 
